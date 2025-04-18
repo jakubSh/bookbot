4922a6319d8e36e75c7d335ad7e54e206f44878d
jakubSh <jakub.majercak1@gmail.com>
2025-04-18 10:25:13 +0200

    added variable to specify book path using sys.argv

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
 from stats import word_count, char_count, sorting, dict_to_list
+import sys
 
 def get_book_text(path):
     with open(path, encoding="latin-1") as f:
         return f.read()
     
 def main():
-    book_text = get_book_text("books/frankenstein.txt")
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_path = sys.argv[1]
+    book_text = get_book_text(book_path)
     num_words = word_count(book_text)
     num_chars = char_count(book_text)
     sorted_chars = dict_to_list(num_chars)
     print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt")
+    print(f"Analyzing book found at {book_path}")
     print("----------- Word Count ----------")
     print(f"Found {num_words} total words")
     print("--------- Character Count -------")
